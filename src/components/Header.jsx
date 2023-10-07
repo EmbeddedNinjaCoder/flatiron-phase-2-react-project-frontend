e8c568e5630e660dc7dcb9118f4d43aa929016ff
German Santos <santos.german@gmail.com>
2023-10-07 14:28:03 -0700

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>My App</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#some-id1">Some Link Title 1</a>
+            </li>
+            <li>
+              <a href="#some-id2">Some Link Title 2</a>
+            </li>
+            <li>
+              <a href="#some-id3">Some Link Title 3</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+export default Header;
